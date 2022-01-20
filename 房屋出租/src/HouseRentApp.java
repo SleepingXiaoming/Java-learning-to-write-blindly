00f146bbfd6ff13b3b39be9f6dc0641c0361ed6c
小明爱睡觉 <73980097+SleepingXiaoming@users.noreply.github.com>
2022-01-20 10:36:19 +0800

    Add files via upload

---
+++ 房屋出租/src/HouseRentApp.java
@@ -0,0 +1,10 @@
+package 小项目.房屋出租.src;
+
+import 小项目.房屋出租.src.view.HouseView;
+
+public class HouseRentApp {
+    public static void main(String[] args) {
+        //创建HouseView对象并显示主菜单
+        new HouseView().mainMenu();;
+    }
+}
